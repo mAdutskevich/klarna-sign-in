da485d53ad9571e06c65db192a0ad02c639c4e97
Mihail Adutskevich <madutskevich@gmail.com>
2025-01-19 01:07:07 +0100

    KLARNA: android/ios build and test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
@@ -23,8 +22,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
